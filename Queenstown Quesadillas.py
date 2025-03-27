2711c2cd1e9205dbb7feb73aeab1a97d9895cb5d
Ezlryb <22lbuchanan@wakatipu.school.nz>
2025-03-27 13:50:22 +1300

    comit 2
    added user interface

--- Queenstown Quesadillas.py
+++ Queenstown Quesadillas.py
@@ -14,6 +14,7 @@ quesadilla_menu = [
     ["Guacamole", 1.50, "A side of fresh, creamy guacamole for dipping or topping."],
     ["Jalapeños", 0.75, "Spice things up with sliced jalapeños, perfect for an extra kick."]
 ]
+order = []
 
 
 def get_int(question, lower, upper):
@@ -40,17 +41,23 @@ def get_int(question, lower, upper):
 
 
 
-def print_menu():
+def menu():
     """Returns the menu as a string"""
     os.system('clear')
-    message = ""
+    message = "\n     **** Queenstown Quesadillas ****\n\n"
     for i, thing in enumerate(quesadilla_menu):
-        if i == 0:
-            message += ("\n     **** Queenstown Quesadillas ****\n\n")
-        elif i == 8:
-            message += ("\n\n    **** Extas menu ****\n\n")
-        message += (f"({i+1}) {thing[0]}  ${thing[1]}\n {thing[2]}\n")
+        if i == 8:
+            message += ("\n\n    **** Extas Menu ****\n\n")
+        message += (f"({i+1}) {thing[0]}  ${thing[1]}\n {thing[2]}\n") # eg (1) Classic Quesadilla
 
+    return message
+
+
+def cart():
+    """Return's ui for cart as well as all your items"""
+    message = "\n\n    **** Cart ****\n\n"
+    for i, item in enumerate(order):
+        message += f"Item {i}: {item[0]} (${item[1]})"
     return message
 
         
@@ -59,7 +66,25 @@ def print_menu():
 def main():
     """While loop with all the user's options"""
     while True:
-        choice = get_int(print_menu() + "\nWhat would you like to order?", 1, 8) - 1 # choice equals the users chosen item's index in the quesadilla_menu list
-        
+        choice = get_int(menu() + "\nWhat would you like to add to your order? (Enter 0 to go to cart!)", 0, 11) - 1 # choice equals the users chosen item's index in the quesadilla_menu list
+        os.system('clear')
+        if choice == -1:
+            if len(order) >= 0:
+                print(cart())
+            else:
+                print("You have no items in your cart!")
+            input("\nPress Enter to Continue\n\n> ")    
+        elif choice <= 7:
+            temp_order = []
+            num = get_int(f"How many {quesadilla_menu[choice][0].lower()}s would you like?", 0, 10)
+            if input(f"Add {num} {quesadilla_menu[choice][0].lower()}(s) ${quesadilla_menu[choice][1]*num} to cart? (y/n)").lower().strip() in ["y", "yes", "yeah", "yup"]:
+                for i in range(num):
+                    temp_order.append(quesadilla_menu[choice][0])
+                    order.append(temp_order)
+                    order.sort()
+        if choice >= 9:
+            get_int(f"{cart()} Which item would you like to add {quesadilla_menu[choice][0]} to?", 1, len(order))
+
+        checkout = get_int(f"{cart} \n\nPress 0 to checkout press 1 to return to menu", 0, 1)
 
 main()
